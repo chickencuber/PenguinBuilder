61c60ddafb409c4c8929d554102fb59d6d4fe9f0
chickencuber <113270822+chickencuber@users.noreply.github.com>
2024-03-24 23:09:23 -0500

    test

--- Docs/main.js
+++ Docs/main.js
@@ -20,12 +20,12 @@ $.all(".category").click(function () {
         const contents = await fetch("./" + f + ".md");
         markdownContent[f] = await contents.text();
     }
-    const converter = new showdown.Converter();
+    const md = markdownit()
 
     for (const [sectionId, markdown] of Object.entries(markdownContent)) {
         const sectionDiv = $(`#${sectionId}`);
         if (sectionDiv) {
-            const htmlContent = converter.makeHtml(markdown);
+            const htmlContent = md.render(markdown);
             sectionDiv.html(htmlContent);
         }
     }
