1792bec49c7ece967b9857192acfb888412b5725
chickencuber <113270822+chickencuber@users.noreply.github.com>
2024-03-24 23:23:49 -0500

    test

--- Docs/main.js
+++ Docs/main.js
@@ -3,7 +3,7 @@ $.all(".category").click(function () {
     const elt = $(id);
     elt.elt.scrollIntoView({ behavior: 'smooth' });
     $("#categories").children.removeClass("selected");
-    $.from(this).class("selected")
+    $.from(this).class("selected");
 });
 
 (async () => {
@@ -31,3 +31,24 @@ $.all(".category").click(function () {
     }
 
 })()
+
+
+$("#doc").on("scroll", () => {
+    function isInViewport(element) {
+        const rect = element.getBoundingClientRect();
+        const html = document.documentElement;
+        return (
+            rect.top >= 0 &&
+            rect.left >= 0 &&
+            rect.bottom <= (window.innerHeight || html.clientHeight) &&
+            rect.right <= (window.innerWidth || html.clientWidth)
+        );
+    }
+    for(const elt of $("#doc").children) {
+        if(isInViewport(elt.elt)) {
+            $("#categories").children.removeClass("selected");
+            $("#button-" + elt.id()).class("selected");
+            return;
+        }
+    }
+});
