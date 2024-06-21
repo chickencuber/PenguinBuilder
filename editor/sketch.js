785e5d8d3009d4b8a5d8e6c7e3dcb90cf1b19348
chickencuber <113270822+chickencuber@users.noreply.github.com>
2024-06-21 17:49:29 -0500

    fixed other bug

--- editor/sketch.js
+++ editor/sketch.js
@@ -360,7 +360,7 @@ const workspace = Blockly.inject("block-editor", {
 
 
 workspace.scale = 0.7;if(!(new URLSearchParams(location.search)).has("test")) {
-  toolbox.getToolboxItems()[1].setDisabled('true');
+  workspace.getToolbox().getToolboxItems()[1].setDisabled('true');
 }
 
 
